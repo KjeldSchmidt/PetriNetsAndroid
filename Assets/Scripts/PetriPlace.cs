fbd1a45d42227f8ed547951636d64942f2997c39
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-16 23:25:56 +0200

    Transitions work, but it's very very non-dynamic.
    ToDo:
    Create tokens at correct position even in setup
    Check to see if input tokens even exist
    Properly space tokens in place
    Find a way to conveniently attach input and output places to transitions

---
+++ Assets/Scripts/PetriPlace.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetriPlace : MonoBehaviour {
+	public int initialTokens;
+	public GameObject token;
+
+	private int tokenCount = 0;
+	private Queue<GameObject> tokens;
+
+	// Use this for initialization
+	void Start () {
+		tokenCount = initialTokens;
+		tokens = new Queue<GameObject>();
+		for ( int i = 0; i < initialTokens; i++ ) {
+			var newToken = Instantiate( token, new Vector3(-4, 0, 0), Quaternion.identity );
+			tokens.Enqueue( newToken );
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void takeToken() {
+		if ( tokenCount > 0 ) {
+			tokenCount--;
+			var oldToken = tokens.Dequeue();
+			Destroy( oldToken );
+		}
+	}
+
+	public void giveToken() {
+		tokenCount++;
+
+		Instantiate( token, transform.position, Quaternion.identity );
+	}
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+	PetriPlace[] inputPlaces;
+	PetriPlace[] outputPlaces;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		PetriPlace input = GameObject.Find("Input").GetComponent<PetriPlace>();
+		PetriPlace output = GameObject.Find("Output").GetComponent<PetriPlace>();
+		inputPlaces = new PetriPlace[] { input };
+		outputPlaces = new PetriPlace[] { output };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		this.fire();
+	}
+
+	void fire() {
+		foreach ( PetriPlace input in inputPlaces ) {
+			input.takeToken();
+		}
+
+		foreach ( PetriPlace output in outputPlaces ) {
+			output.giveToken();
+		}
+	}
+}
