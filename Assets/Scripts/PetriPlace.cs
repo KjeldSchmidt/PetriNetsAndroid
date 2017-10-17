dc409292713dcbed5ef57046e893aed1fac3becb
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-17 19:33:48 +0200

    Tokens now automatically stop overlapping
    Nothing keeps them inside the circle, though.

--- Assets/Scripts/PetriPlace.cs
+++ Assets/Scripts/PetriPlace.cs
@@ -9,19 +9,20 @@ public class PetriPlace : MonoBehaviour {
 	private int tokenCount = 0;
 	private Queue<GameObject> tokens;
 
+	private static float tokenRadius = 0.74f;
+
 	// Use this for initialization
 	void Start () {
 		tokenCount = initialTokens;
 		tokens = new Queue<GameObject>();
 		for ( int i = 0; i < initialTokens; i++ ) {
-			var newToken = Instantiate( token, new Vector3(-4, 0, 0), Quaternion.identity );
-			tokens.Enqueue( newToken );
+			createNewToken();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		spaceTokens();
 	}
 
 	public void takeToken() {
@@ -34,7 +35,25 @@ public class PetriPlace : MonoBehaviour {
 
 	public void giveToken() {
 		tokenCount++;
+		createNewToken();
+	}
 
-		Instantiate( token, transform.position, Quaternion.identity );
+	private void createNewToken() {
+		Vector3 randomOffset = Random.insideUnitCircle * 0.01f;
+		tokens.Enqueue( Instantiate( token, transform.position + randomOffset, Quaternion.identity ) );
+	}
+
+	private void spaceTokens() {
+		foreach( GameObject currentToken in tokens ) {
+			foreach( GameObject compareToken in tokens ) {
+				if ( !Object.ReferenceEquals( currentToken, compareToken ) ) {
+					Vector3 displacement = compareToken.transform.position - currentToken.transform.position;
+					float magnitude = displacement.magnitude;
+					if ( magnitude < tokenRadius ) {
+						currentToken.transform.position += -1 * displacement * ( ( tokenRadius - magnitude ) / tokenRadius );
+					}
+				}
+			}
+		}
 	}
 }
