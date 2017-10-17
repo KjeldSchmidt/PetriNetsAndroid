33f74e396bc75410b97d22e14d484af055abfd2d
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-17 21:31:33 +0200

    Ammount of placeable tokens is shown
    However, they can't be placed or depleted in any way yet.

---
+++ Assets/Scripts/PlaceableToken.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableToken : MonoBehaviour {
+
+	public int numberOfAvailabeTokens;
+	public GameObject tokenPrefab;
+
+	private Stack<GameObject> placeableTokens = new Stack<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+		for ( int i = 1; i < numberOfAvailabeTokens; i++ ) {
+			Vector3 offset = new Vector3(i, 0, 0);
+			placeableTokens.Push( Instantiate( tokenPrefab, transform.position + offset, Quaternion.identity ) );
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
