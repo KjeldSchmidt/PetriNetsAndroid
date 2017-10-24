107532e4607efc0823d4b9053c250b6bad141d68
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-24 20:26:41 +0200

    Added first iteration of wincondition
    Allows you to select any number of places to be completely empty, then
    progresses to the next level instantly.

---
+++ Assets/Levels/Wincondition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wincondition : MonoBehaviour {
+	public PetriPlace[] placesToBeEmpty;
+	public Object nextLevel;
+	public bool haveToUseAllTokens;
+	private PlaceableTokenController tokenController;
+
+	// Use this for initialization
+	void Start () {
+		tokenController = GameObject.FindWithTag("PlaceableTokenController").GetComponent<PlaceableTokenController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool winning = true;
+		foreach ( PetriPlace place in placesToBeEmpty ) {
+			if ( place.hasToken() ) {
+				winning = false;
+			}
+		}
+
+		
+
+
+
+
+
+		if ( winning ) {
+			SceneManager.LoadScene(nextLevel.name, LoadSceneMode.Single);
+		}
+	}
+}
