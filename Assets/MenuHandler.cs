29f1578a385dae5dcc2b939d4de5b3855460972a
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-24 20:06:28 +0200

    Added very basic start menu, to see how it works

---
+++ Assets/MenuHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Level1() {
+		SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+	}
+}
