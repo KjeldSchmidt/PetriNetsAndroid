771f412a710adbaecc2d60d78aad8c2b1ac1ebba
Kjeld Schmidt <s_kjeld@hotmail.de>
2017-10-30 15:58:32 +0100

    First work on automatically placed arrows
    Scale is set to a reasonable (but not entirely correct*) value
    automatically. Position is not yet set in a reasonable way
    
    *Scale is set to be the full distance from the inputs center to the
    outputs center, but it should be set so that it only goes to the outputs
    outer perimeter.

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+	public PetriPlace place;
+	public Transition transition;
+	public Direction direction;
+
+	public enum Direction {In, Out};
+
+	private Transform input;
+	private Transform output;
+	private Transform arrowLine;
+
+	private float baseLength = 2.0f; // This is the length of the arrowLine, excluding the head(!), before scaling is applied. This depends on illustrator settings.
+
+	private float absoluteLength;
+	private float scale;
+	private Vector3 absoluteStartPosition; // This is equal to the center of the input to this arrow (a place for input, transition for output)
+	private Vector3 absoluteEndPosition;   // This is equal to the center of the output of this arrow
+	private Vector3 EndPosition;           // This is where the arrow 
+
+
+	// Use this for initialization
+	void Start () {
+		arrowLine = transform.GetChild(1);
+		input = (direction == Direction.In) ? place.transform : transition.transform;
+		output = (direction == Direction.Out) ? place.transform : transition.transform;
+		absoluteLength = Vector3.Distance(input.position, output.position);
+		scale = absoluteLength/baseLength;
+
+		arrowLine.localScale = new Vector3(scale, 1, 1);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
